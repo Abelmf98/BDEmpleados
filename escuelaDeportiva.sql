fabd454b717ed9a7f97c54520fb9f4f9af245d21
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2021-10-15 13:39:45 +0200

    Add files via upload

--- escuelaDeportiva.sql
+++ escuelaDeportiva.sql
@@ -2,9 +2,9 @@
 CREATE DATABASE BDEmpleados;
 
 CREATE TABLE Empleados(
-  IdEmpleado smallint UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  IdEmpleado tinyint UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   Nombre varchar (50) NOT NULL,
   Correo varchar(100) UNIQUE NULL,
-  Telefono varchar(9) NOT NULL,
+  Telefono char (9) NOT NULL,
   DNI char(9) UNIQUE NOT NULL
 );
